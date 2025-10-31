076b64e68fdb09040e5a2f20b1f680950a2bb93d
demss <demsscash@gmail.com>
2025-10-31 17:04:20 +0000

    j

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { LoginForm } from '@/components/auth/LoginForm'
+
+export default function AuthPage() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-green-50 to-blue-50 flex items-center justify-center p-4">
+      <div className="w-full max-w-md">
+        <LoginForm />
+      </div>
+    </div>
+  )
+}
